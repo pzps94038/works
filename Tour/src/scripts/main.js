2a84f76b6ecd6bed3e24d5895fc180c04bfd3b3c
Erica <erica@Ericade-iMac.local>
2020-04-30 01:59:03 +0800

    import data

--- Tour/src/scripts/main.js
+++ Tour/src/scripts/main.js
@@ -1,1 +1,65 @@
-console.log('aaa')
+const getElemt = function(elemt) {
+    return document.querySelector(elemt);
+}
+
+const xhr = new XMLHttpRequest();
+xhr.open('get', 'https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97', true)
+xhr.send(null);
+xhr.onload = function(){
+    const str = JSON.parse(xhr.responseText)
+    const data = str.result.records;
+    const cards = getElemt('.card-box');
+    let cont = '';
+    for( let i = 0; i < data.length; i++){
+        cont += `
+        <li class="card-item">
+            <div class="card-img">
+                <div class="card-txt">
+                    <div class="name">${data[i].Name}</div>
+                    <div class="zone">${data[i].Zone}</div>
+                </div><img src="${data[i].Picture1}" alt="">
+            </div>
+            <div class="card-cont">
+                <p class="opentime">
+                    <i class="material-icons">watch_later</i><span>${data[i].Opentime}</span></p>
+                <p class="add">
+                    <i class="material-icons">place</i><a href="https://www.google.com.tw/maps/search/${data[i].Name}" target="_blank">${data[i].Add}</a>
+                </p>
+                <p class="tel"><i class="material-icons">smartphone</i><a href="tel:${data[i].Tel}">+${data[i].Tel}</a></p>
+                <p class="ticketinfo"><i class="material-icons">local_offer</i>${data[i].Ticketinfo}</p>
+            </div>
+        </li>`
+    }
+    cards.innerHTML = cont;
+
+
+
+    var ZoneList = [];
+    for(let i=0; i < data.length; i++) {
+        ZoneList.push(data[i].Zone);
+    }
+    var zone = Array.from(new Set(ZoneList));
+    console.log(zone); 
+    let select = '';
+    const selecter = getElemt('.select-wrap ol');
+    for(let i=0; i<zone.length; i++) {
+        select += `
+        <li><input type="radio" id="list[${i+1}]" name="city"><label class="select-option" for="list[${i+1}]">${zone[i]}</label></li>`
+    }
+    selecter.innerHTML = select;
+    
+    
+    
+}
+
+// ----- Scroll Top ----- //
+const scrollBtn = getElemt('#scroll-top');
+const scroll_top = function(){
+    // let targetPosition = scrollBtn.getBoundingClientRect().top;
+    // let starPosition = window.pageYOffset;
+    // let distance = targetPosition - starPosition;
+    // window.scrollTo(0, '-' + distance);
+    window.scrollTo(0, 0);
+}
+
+scrollBtn.addEventListener('click', scroll_top);
--- Tour/webroot/scripts/all.js
+++ Tour/webroot/scripts/all.js
@@ -1,4 +1,52 @@
 "use strict";
 
-console.log('aaa');
+var getElemt = function getElemt(elemt) {
+  return document.querySelector(elemt);
+};
+
+var xhr = new XMLHttpRequest();
+xhr.open('get', 'https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97', true);
+xhr.send(null);
+
+xhr.onload = function () {
+  var str = JSON.parse(xhr.responseText);
+  var data = str.result.records;
+  var cards = getElemt('.card-box');
+  var cont = '';
+
+  for (var i = 0; i < data.length; i++) {
+    cont += "\n        <li class=\"card-item\">\n            <div class=\"card-img\">\n                <div class=\"card-txt\">\n                    <div class=\"name\">".concat(data[i].Name, "</div>\n                    <div class=\"zone\">").concat(data[i].Zone, "</div>\n                </div><img src=\"").concat(data[i].Picture1, "\" alt=\"\">\n            </div>\n            <div class=\"card-cont\">\n                <p class=\"opentime\">\n                    <i class=\"material-icons\">watch_later</i><span>").concat(data[i].Opentime, "</span></p>\n                <p class=\"add\">\n                    <i class=\"material-icons\">place</i><a href=\"https://www.google.com.tw/maps/search/").concat(data[i].Name, "\" target=\"_blank\">").concat(data[i].Add, "</a>\n                </p>\n                <p class=\"tel\"><i class=\"material-icons\">smartphone</i><a href=\"tel:").concat(data[i].Tel, "\">+").concat(data[i].Tel, "</a></p>\n                <p class=\"ticketinfo\"><i class=\"material-icons\">local_offer</i>").concat(data[i].Ticketinfo, "</p>\n            </div>\n        </li>");
+  }
+
+  cards.innerHTML = cont;
+  var ZoneList = [];
+
+  for (var _i = 0; _i < data.length; _i++) {
+    ZoneList.push(data[_i].Zone);
+  }
+
+  var zone = Array.from(new Set(ZoneList));
+  console.log(zone);
+  var select = '';
+  var selecter = getElemt('.select-wrap ol');
+
+  for (var _i2 = 0; _i2 < zone.length; _i2++) {
+    select += "\n        <li><input type=\"radio\" id=\"list[".concat(_i2 + 1, "]\" name=\"city\"><label class=\"select-option\" for=\"list[").concat(_i2 + 1, "]\">").concat(zone[_i2], "</label></li>");
+  }
+
+  selecter.innerHTML = select;
+}; // ----- Scroll Top ----- //
+
+
+var scrollBtn = getElemt('#scroll-top');
+
+var scroll_top = function scroll_top() {
+  // let targetPosition = scrollBtn.getBoundingClientRect().top;
+  // let starPosition = window.pageYOffset;
+  // let distance = targetPosition - starPosition;
+  // window.scrollTo(0, '-' + distance);
+  window.scrollTo(0, 0);
+};
+
+scrollBtn.addEventListener('click', scroll_top);
 //# sourceMappingURL=all.js.map
